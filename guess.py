dcaaa734f5d9fc8ede45adaf5780beefeea2ebee
Gary Zheng <garyz1997@yahoo.com>
2015-04-14 10:12:02 -0400

    randomguess solved

--- guess.py
+++ guess.py
@@ -9,4 +9,5 @@ while i > 0:
     random.random() #to mix it up a little
     i-=1
 key=math.floor(random.random()*1000000)
-os.system('echo '+str(key+' | nc stuyctf.me 34567')
+#os.system('echo ' + str('nc stuyctf.me 34567'))
+os.system('echo '+str(key)+' | nc stuyctf.me 34567')
