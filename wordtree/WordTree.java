3d5dd58df900f1716e384fbeee75c49c4f7927d0
Gary Zheng <garyz1997@yahoo.com>
2015-04-30 10:15:48 -0400

    dict

--- wordtree/WordTree.java
+++ wordtree/WordTree.java
@@ -1,14 +1,33 @@
 public class WordTree
 {
-    ArrayList<String> Dict;
-    
 
-
-    
-    public static void main(String args[])
-    {
-
-    }
-
-
+	Arraylist<String> Dict;
+	TreeNode root;
+	
+	public ArrayList<String> loadDictionary() 
+	{
+		String s = "zzz";
+		ArrayList<String> dictionary = new ArrayList<String>();
+		try 
+		{
+			FileReader f = new FileReader("wordlist.txt");
+			BufferedReader b = new BufferedReader(f);
+			while( s != null ) 
+			{
+				s = b.readLine();
+				if ( s != null )
+					dictionary.add(s);
+			}
+		}
+		catch (IOException e)
+		return dictionary;
+	}
+	
+	
+    //	Dict.add(")
+	
+	public WordTree() 
+	{
+		
+	}
 }
